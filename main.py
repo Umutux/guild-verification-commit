6feed45087f4d381f378d1d526ff3f0c05472b3b
QuantummGm <136803646+Umutux@users.noreply.github.com>
2025-11-04 21:33:30 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-04T18:33:27.616Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-04T18:33:29.257Z
+Commit: Introduce real-time notifications

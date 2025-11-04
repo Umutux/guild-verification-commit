23e998cf64434554732f4eec73f9d3058267b417
QuantummGm <136803646+Umutux@users.noreply.github.com>
2025-11-04 21:33:57 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-04T18:33:41.649Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-04T18:33:56.280Z
+Commit: Introduce real-time notifications

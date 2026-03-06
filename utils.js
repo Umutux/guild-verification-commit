a1d11d8b5817842ea8595be7ab123cc9e88feb73
QuantummGm <136803646+Umutux@users.noreply.github.com>
2026-03-06 10:36:06 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:36:03.825Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T07:36:05.453Z
+Commit: Resolve null pointer exception in user service

bc6c8e13de9169a6b9be3d041d210938270662b3
QuantummGm <136803646+Umutux@users.noreply.github.com>
2026-03-06 10:40:08 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:39:31.112Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T07:40:07.580Z
+Commit: Resolve null pointer exception in user service

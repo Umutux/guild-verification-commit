00363b2aa7c978847346129260e9e4e1b34083c8
QuantummGm <136803646+Umutux@users.noreply.github.com>
2026-03-06 10:35:46 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:35:35.910Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T07:35:46.173Z
+Commit: Fix CSS styling bug on mobile devices

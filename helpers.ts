52f571a54dde823393f06c89c9411916518e52db
QuantummGm <136803646+Umutux@users.noreply.github.com>
2026-03-06 10:34:52 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:34:44.934Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T07:34:52.335Z
+Commit: Fix broken links in documentation

f959979b5910608a0a3d84813a7b8ce5c9ea87b6
QuantummGm <136803646+Umutux@users.noreply.github.com>
2026-03-06 10:35:20 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:34:57.221Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T07:35:20.251Z
+Commit: Fix memory leak in event handlers
